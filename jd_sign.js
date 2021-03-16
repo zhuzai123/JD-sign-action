cee7a95c501361d32835b3189b9d2f4aaca9664f
HHZ <849513883@qq.com>
2021-03-16 10:14:41 +0800

    Update jd_sign.js

--- jd_sign.js
+++ jd_sign.js
@@ -62,38 +62,6 @@ function setupCookie() {
 }
 
 function sendNotificationIfNeed() {
-  var Email = {
-    send: function (a) {
-        return new Promise(function (n, e) {
-            a.nocache = Math.floor(1e6 * Math.random() + 1), a.Action = "Send"; var t = JSON.stringify(a);
-            Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) { n(e) })
-        })
-    },
-    ajaxPost: function (e, n, t) {
-        var a = Email.createCORSRequest("POST", e); a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), a.onload = function () { var e = a.responseText; null != t && t(e) }, a.send(n)
-    },
-    ajax: function (e, n) {
-        var t = Email.createCORSRequest("GET", e); t.onload = function () { var e = t.responseText; null != n && n(e) }, t.send()
-    },
-    createCORSRequest: function (e, n) {
-        var t = new XMLHttpRequest;
-        return "withCredentials" in t ? t.open(e, n, !0) : "undefined" != typeof XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null, t
-    }
-  };
-  
-  Email.send({
-      Host : "smtp.gmail.com",
-      Username : "hehouzhu@gmail.com",
-      Password : "uduzxmbfnbyibxde",
-      To : 'hehouzhu@qq.com',
-      From : "Houzhu He",
-      Subject : "测试案例",
-      Body : "收到请回复"
-
-  }).then(
-    message => alert(message)
-  );
-  
   if (!push_key) {
     console.log('执行任务结束!'); return;
   }
